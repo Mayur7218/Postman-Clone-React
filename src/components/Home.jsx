8f311b65099b9a5f20e0e610095963b4b4d2edf2
Mayur7218 <mjjadhav7218@gmail.com>
2024-06-02 01:00:36 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,66 @@
+import { useContext, useState } from 'react';
+
+import { Box } from '@mui/material';
+import { makeStyles } from "@mui/styles";
+
+import { DataContext } from '../context/DataProvider';
+import { checkParams } from '../utils/common-utils';
+import { getData } from '../service/api';
+
+//components
+import Form from "./Form";
+import SelectTab from './SelectTab';
+import SnackBar from './SnackBar';
+import Header from './Header';
+import Response from './Response';
+import ErrorScreen from './ErrorScreen';
+
+const useStyles = makeStyles({
+    component: {
+        width: '60%',
+        margin: '20px auto 0 auto'
+    }
+})
+
+const Home = () => {
+    const classes = useStyles();
+    
+    const [error, setError] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('')
+    const [errorResponse, setErrorResponse] = useState(false);
+    const [apiResponse, setApiResponse] = useState({})
+
+    const { formData, jsonText, paramData, headerData } = useContext(DataContext);
+    
+
+    const onSendClick = async () => {
+        if(!checkParams(formData, jsonText, paramData, headerData, setErrorMsg)) {
+            setError(true);
+            return false;
+        }
+
+        let response = await getData(formData, jsonText, paramData, headerData);
+        console.log(response);
+        if (response === 'error') {
+            setErrorResponse(true);
+            return;
+        }
+        setErrorResponse(false);
+        setApiResponse(response.data)
+    }
+
+    return (
+        <>
+            <Header />
+            <Box className={classes.component}>
+                <Form onSendClick={onSendClick} />
+                <SelectTab />
+                { errorResponse ? <ErrorScreen /> : <Response data={apiResponse} /> }
+                { error && <SnackBar errorMsg={errorMsg} error={error} setError={setError} /> }
+            </Box>
+            
+        </>
+    )
+}
+
+export default Home;
